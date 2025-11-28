4ae1b2b9c3020e9cf2d554f7c1f59cb8bdfbe8af
Danny23b <159599919+Danny23b@users.noreply.github.com>
2025-11-28 15:45:12 -0600

    Create header.php

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Mi App CRUD</title>
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+<div class="container">
+    <header>
+        <img src="assets/logo.png" class="logo">
+        <h1>Mi Aplicación CRUD</h1>
+    </header>
