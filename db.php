83f582a0a842b2ebb5d59b0b4fcaa4de60647895
Danny23b <159599919+Danny23b@users.noreply.github.com>
2025-11-28 15:56:02 -0600

    Update db.php

--- db.php
+++ db.php
@@ -3,7 +3,7 @@ $host = getenv("dpg-d4l1csjuibrs73fpnic0-a");
 $dbname = getenv("my_app_zwdv");
 $user = getenv("my_app_zwdv_user");
 $pass = getenv("IiLLuxOd12oITw9JaM34ZC7jdMDDriUM");
-$port = getenv("5432");
+$port = 5432;
 
 try {
     $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $pass);
