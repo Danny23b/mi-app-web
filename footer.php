37b40b94bea37f45f67c411a248a6bdb9bd9e31e
Danny23b <159599919+Danny23b@users.noreply.github.com>
2025-11-28 15:46:53 -0600

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,9 @@
+<footer>
+    <p>Curso: Conceptualización de servicios en la nube</p>
+    <p>Alumno: Edgar Jimenez</p>
+    <p>Código: 216590045</p>
+    <p>Contacto: edgar.jimenez23b@udgvirtual.udg.mx</p>
+</footer>
+</div>
+</body>
+</html>
