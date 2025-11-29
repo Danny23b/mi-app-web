3e076ab5c688f22cb71782c04bd5b4219171edc6
Danny23b <159599919+Danny23b@users.noreply.github.com>
2025-11-28 18:04:49 -0600

    Update db.php

--- db.php
+++ db.php
@@ -8,7 +8,6 @@ $port = getenv("DB_PORT");
 try {
     $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Conexión exitosa";
 } catch (PDOException $e) {
     die("Error en la conexión: " . $e->getMessage());
 }
