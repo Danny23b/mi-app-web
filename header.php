a92bc05f483f83d94051fab3e905ef3e4effb8df
Danny23b <159599919+Danny23b@users.noreply.github.com>
2025-11-28 18:07:27 -0600

    Update header.php

--- header.php
+++ header.php
@@ -8,6 +8,6 @@
 <body>
 <div class="container">
     <header>
-        <img src="assets/logo.png" class="logo">
+        <img src="https://th.bing.com/th/id/OIP.3LNOr4NOhPB84q3NUi0CRQHaD4?o=7rm=3&rs=1&pid=ImgDetMain&o=7&rm=3" class="logo">
         <h1>Mi Aplicación CRUD</h1>
     </header>
